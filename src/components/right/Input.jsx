06d836e69f71ff771b823d0bd971b18608ec8400
BELINDA <kennyegun240@gmail.com>
2022-12-04 17:23:54 +0100

    Compnent for inputing messages to be sent

---
+++ src/components/right/Input.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Input = () => {
+  return (
+    <div className='inputContainer'>
+        <input type="text" placeholder='Type a message...' />
+    </div>
+  )
+}
+
+export default Input
