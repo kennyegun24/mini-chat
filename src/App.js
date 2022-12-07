1d3a03d32b1cc7f5b879f4b4c6bec2abdc982e45
BELINDA <kennyegun240@gmail.com>
2022-12-07 20:29:07 +0100

    Modify

--- src/App.js
+++ src/App.js
@@ -7,9 +7,10 @@ import { useContext } from 'react';
 import { AuthContext } from './context/context';
 import { BrowserRouter } from 'react-router-dom';
 function App() {
+
+  // CALL THE USECONTEXT HOOK AND PASS AUTHCONTEXT FROM CONTEXT.JS STORE AS A PARAMETER TO CHECK FOR USER STATUS
   const {currentUser} = useContext(AuthContext)
 
-i
   return (
     <BrowserRouter>
       <Routes>
