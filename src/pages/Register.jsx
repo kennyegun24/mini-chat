1ef246898c058efc6f478e93f1ac2cf1a1839613
BELINDA <kennyegun240@gmail.com>
2022-12-07 20:32:55 +0100

    Changed format to set docs and initialize storage

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -7,9 +7,12 @@ import { doc, setDoc } from "firebase/firestore";
 
 const Register = () => {
 
+  // SET ERROR
   const [err, setErr] = useState(false)
+  // INITIALIZE USENAVIGATE HOOK
   const navigation = useNavigate()
 
+  // FUNCTION TO SUBMIT BASED ON SUBMIT
   const submitInput = async (e) => {
     e.preventDefault()
     const displayName = e.target[0].value;
@@ -18,47 +21,77 @@ const Register = () => {
     const file = e.target[3].files[0]
 
     try {
+      // IMPORT AND AWAIT THE FUNCTION TO CREATE AN ACCOUNT BY PASSING THE EMAIL AND PASSWORD AS PARAMETERS
       const response = await createUserWithEmailAndPassword(auth, email, password)
+      // THIS IS TO INITIATE FIREBASE STORAGE
       const storageRef = ref(storage, response.user.uid);
-      const uploadTask = uploadBytesResumable(storageRef, file);
+      // THIS IS TO STORE FILES IN THE FIREBASE STORAGE
 
-      uploadTask.on(
-        (error) => {
-          setErr(true)
-        }, 
-          () => {
-            getDownloadURL(uploadTask.snapshot.ref).then( async (downloadURL) => {
-              await updateProfile(response.user, {
-                displayName,
-                photoURL: downloadURL,
-              });
+      await uploadBytesResumable(storageRef, file).then(()=> {
+        getDownloadURL(storageRef).then(async(downloadURL)=> {
+          try {
+            await updateProfile(response.user, {
+              displayName,
+              photoURL: downloadURL,
+            });
+            // THIS IS TO STORE NEW REGS COLLECTIONS TO DATABASE
             await setDoc(doc(db, "users", response.user.uid),{
               uid: response.user.uid,
               displayName,
               email,
               photoURL: downloadURL,
             });
-            // await setDoc(doc(db, "userchat", response.user.uid, {}))
+            // THIS IS TO STORE AND INITIATE NEW REGS CHATS COLLECTIONS IN DATABASE
+            await setDoc(doc(db, "userchat", response.user.uid), {});
             navigation('/');
-            });
+
+          }catch(err) {
+            setErr(true)
           }
-        );
-      console.log(uploadTask)
+        })
+      })
+      // const uploadTask = uploadBytesResumable(storageRef, file);
+
+      // uploadTask.on(
+      //   (error) => {
+      //     setErr(true)
+      //   },
+      //     () => {
+      //       // THIS IS TO STORE NEW REGS TO STORAGE
+      //       getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+      //         await updateProfile(response.user, {
+      //           displayName,
+      //           photoURL: downloadURL,
+      //         });
+      //         // THIS IS TO STORE NEW REGS COLLECTIONS TO DATABASE
+      //         await setDoc(doc(db, "users", response.user.uid),{
+      //           uid: response.user.uid,
+      //           displayName,
+      //           email,
+      //           photoURL: downloadURL,
+      //         });
+      //         // THIS IS TO STORE AND INITIATE NEW REGS CHATS COLLECTIONS IN DATABASE
+      //         await setDoc(doc(db, "userchat", response.user.uid), {});
+      //         navigation('/');
+      //       });
+      //     }
+      //   );
+      // console.log(uploadTask)
     } catch(err) {
       setErr(true)
-    }
+    };
 
-  }
+  };
 
   return ( 
     <div className='loginContainer'>
     <div className='formWrapper'>
       <h3>Register</h3>
-      <form onSubmit={submitInput}>
-        <input className='formInput' type="name" placeholder='name'/>
-        <input type="email" className='formInput' placeholder='enter your email'/>
-        <input type="password" className='formInput' placeholder='enter your password...'/>
-        <input type="file" id="file" style={{display:"none"}} />
+      <form className='form' onSubmit={submitInput}>
+        <input className='formInput' type="name" placeholder='name' required/>
+        <input type="email" className='formInput' placeholder='enter your email' required/>
+        <input type="password" className='formInput' placeholder='enter your password...' required/>
+        <input type="file" id="file" style={{display:"none"}} required />
         <label htmlFor="file">
           <span>
             put an image
