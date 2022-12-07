8f787b0349624f6698e3c6d035cce7f5060d3d11
BELINDA <kennyegun240@gmail.com>
2022-12-07 20:34:28 +0100

    Add functionality to every component in the right part of the app

--- src/components/right/Topnav.jsx
+++ src/components/right/Topnav.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { FaEllipsisH } from "react-icons/fa";
-import { FaUserPlus } from "react-icons/fa";
 import { FaVideo } from "react-icons/fa";
-
+import { FaPhone } from "react-icons/fa";
+import { ChatContext } from '../../context/chatsContext';
 
 const Topnav = () => {
+  const {data} = useContext(ChatContext)
+  console.log(data)
   return (
     <div className='topnavContainer'>
         <div className='topnavWrapper'>
             <p>
-                Julius
+                {data.user?.displayName}
             </p>
             <div className="topnavWrapSm">
+                <FaPhone />
                 <FaVideo />
-                <FaUserPlus />
                 <FaEllipsisH />
             </div>
         </div>
