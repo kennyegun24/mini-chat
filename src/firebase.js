76b9eca63b323dfb42b0b2190d33dc09baab4f79
BELINDA <kennyegun240@gmail.com>
2022-12-07 20:29:57 +0100

    Modify

--- src/firebase.js
+++ src/firebase.js
@@ -3,33 +3,7 @@ import {getAuth} from "firebase/auth"
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDlDeyOs4lpT2Ny5S_wg6UGs3aE3NMbNKM",
-//   authDomain: "chat-f7b52.firebaseapp.com",
-//   projectId: "chat-f7b52",
-//   storageBucket: "chat-f7b52.appspot.com",
-//   messagingSenderId: "826911440420",
-//   appId: "1:826911440420:web:2d0942be025b38603462b1"
-// };
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCiCg3E2pnC9vWBXjrn-YIfLmShDWpwoi4",
-//   authDomain: "chat2-5afa5.firebaseapp.com",
-//   projectId: "chat2-5afa5",
-//   storageBucket: "chat2-5afa5.appspot.com",
-//   messagingSenderId: "529592359836",
-//   appId: "1:529592359836:web:bfaf9d7af0775761270659"
-// };
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBBRuEdMx26wccsv7mwXDiMkpomZN2R6JA",
-//   authDomain: "chat3-995a6.firebaseapp.com",
-//   projectId: "chat3-995a6",
-//   storageBucket: "chat3-995a6.appspot.com",
-//   messagingSenderId: "90890848812",
-//   appId: "1:90890848812:web:63798035e303936549df89"
-// };
-
+// KEYS AND INFOS ABOUT MY FIREBASE APP
 const firebaseConfig = {
   apiKey: "AIzaSyC0RXO1Xu2YzwqUTQvW4c5w_bEKC_D1qA8",
   authDomain: "chat-eb001.firebaseapp.com",
@@ -43,5 +17,8 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth()
 
+// INITIATE FIREBASE STORAGE
 export const storage = getStorage();
+
+// INITIATE FIRESTORE DATABASE
 export const db = getFirestore()
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { ChatContextProvider } from './context/chatsContext';
 import { AuthContextProvider } from './context/context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+
+  // WRAP APP WITH THE authContextProvider FROM CONTEXT.JS FILE
   <AuthContextProvider>
-    <React.StrictMode>
+    <ChatContextProvider>
+      <React.StrictMode>
         <App />
-    </React.StrictMode>
+      </React.StrictMode>
+    </ChatContextProvider>
   </AuthContextProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
